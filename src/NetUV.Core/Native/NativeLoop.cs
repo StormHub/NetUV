corefan/NetworkCore

Created handles and requests.

src/NetworkCore.Libuv/Native/NativeLoop.cs
@@ -1,0 +1,210 @@
+﻿// ReSharper disable InconsistentNaming
+
+namespace NetworkCore.Libuv.Native
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    enum uv_run_mode
+    {
+        UV_RUN_DEFAULT = 0,
+        UV_RUN_ONCE,
+        UV_RUN_NOWAIT
+    };
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct uv_loop_t
+    {
+        /* User data - use this for whatever. */
+        public IntPtr data;
+
+        /* Loop reference counting. */
+        public uint active_handles;
+    }
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate void uv_walk_cb(IntPtr handle, IntPtr arg);
+
+    static partial class NativeMethods
+    {
+        internal static int GetLoopSize()
+        {
+            IntPtr value = uv_loop_size();
+            int size = value.ToInt32();
+            if (size <= 0)
+            {
+                throw new InvalidOperationException("Loop handle size must be greater than zero.");
+            }
+
+            return size;
+        }
+
+        internal static void InitializeLoop(IntPtr handle)
+        {
+            if (handle == IntPtr.Zero)
+            {
+                throw new ArgumentException("Invalid empty handle value.", nameof(handle));
+            }
+
+            Invoke(uv_loop_init, handle);
+        }
+
+        internal static bool CloseLoop(IntPtr handle)
+        {
+            if (handle == IntPtr.Zero)
+            {
+                return false;
+            }
+
+            int result = uv_loop_close(handle);
+            if (result >= 0)
+            {
+                return true;
+            }
+
+            var code = (uv_err_code)result;
+            if (code == uv_err_code.UV_EBUSY)
+            {
+                return false;
+            }
+
+            throw CreateError(code);
+        }
+
+        internal static void WalkLoop(IntPtr handle, uv_walk_cb callback)
+        {
+            if (handle == IntPtr.Zero 
+                || callback == null)
+            {
+                return;
+            }
+
+            InvokeAction(uv_walk, handle, callback, handle);
+        }
+
+        internal static int RunLoop(IntPtr handle, uv_run_mode mode)
+        {
+            if (handle == IntPtr.Zero)
+            {
+                throw new ArgumentException("Empty handle value is not valid.");
+            }
+
+            /*
+              UV_RUN_DEFAULT: 
+                Runs the event loop until there are no more active and referenced handles or requests.
+                Returns non-zero if uv_stop() was called and there are still active handles or requests.
+                Returns zero in all other cases.
+
+              UV_RUN_ONCE: 
+                Poll for i/o once. Note that this function blocks if there are no pending callbacks.
+                Returns zero when done (no active handles or requests left), 
+                or non-zero if more callbacks are expected(meaning you should run the event loop again sometime in the future).
+
+              UV_RUN_NOWAIT: 
+                Poll for i/o once but don’t block if there are no pending callbacks.
+                Returns zero if done(no active handles or requests left), 
+                or non-zero if more callbacks are expected(meaning you should run the event loop again sometime in the future).
+            */
+
+            int result = InvokeFunction(uv_run, handle, mode);
+            Log.Debug($"Native run loop {handle} {mode}, result = {result}");
+            return result;
+        }
+
+        internal static void StopLoop(IntPtr handle)
+        {
+            if (handle == IntPtr.Zero)
+            {
+                throw new ArgumentException("Empty handle value is not valid.");
+            }
+
+            InvokeAction(uv_stop, handle);
+        }
+
+        internal static bool IsLoopAlive(IntPtr handle)
+        {
+            if (handle == IntPtr.Zero)
+            {
+                throw new ArgumentException("Empty handle value is not valid.");
+            }
+
+            int result = InvokeFunction(uv_loop_alive, handle);
+            return result != 0;
+        }
+
+        internal static long LoopNow(IntPtr handle)
+        {
+            if (handle == IntPtr.Zero)
+            {
+                throw new InvalidOperationException("Invalid empty handle value.");
+            }
+
+            return InvokeFunction(uv_now, handle);
+        }
+
+        internal static long LoopNowInHighResolution(IntPtr handle)
+        {
+            if (handle == IntPtr.Zero)
+            {
+                throw new InvalidOperationException("Invalid empty handle value.");
+            }
+
+            return InvokeFunction(uv_hrtime, handle);
+        }
+
+        internal static int GetBackendTimeout(IntPtr handle)
+        {
+            if (handle == IntPtr.Zero)
+            {
+                throw new InvalidOperationException("Invalid empty handle value.");
+            }
+
+            // The return value is in milliseconds, or -1 for no timeout.
+            int timeout = InvokeFunction(uv_backend_timeout, handle);
+            return timeout > 0 ? timeout : 0;
+        }
+
+        internal static void LoopUpdateTime(IntPtr handle)
+        {
+            if (handle == IntPtr.Zero)
+            {
+                throw new ArgumentException("Empty handle value is not valid.");
+            }
+
+            InvokeAction(uv_update_time, handle);
+        }
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        static extern int uv_loop_init(IntPtr handle);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        static extern void uv_stop(IntPtr handle);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        static extern int uv_loop_close(IntPtr handle);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        static extern int uv_loop_alive(IntPtr handle);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        static extern int uv_run(IntPtr handle, uv_run_mode mode);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        static extern IntPtr uv_loop_size();
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        static extern void uv_update_time(IntPtr handle);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        static extern long uv_now(IntPtr handle);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        static extern long uv_hrtime(IntPtr handle);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        static extern int uv_backend_timeout(IntPtr handle);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        static extern void uv_walk(IntPtr handle, uv_walk_cb walk_cb, IntPtr arg);
+    }
+}

test/NetworkCore.Libuv.Tests/AsyncTests.cs
@@ -1,0 +1,76 @@
+﻿namespace NetworkCore.Libuv.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using NetworkCore.Libuv.Handles;
+    using Xunit;
+
+    public sealed class AsyncTests : IDisposable
+    {
+        Loop loop;
+        Prepare prepare;
+        Async async;
+        Task task;
+
+        int prepareCalled;
+        int asyncCalled;
+
+        void AsyncSend()
+        {
+            while (this.asyncCalled < 3)
+            {
+                this.async.Send();
+            }
+        }
+
+        void PrepareCallback(Prepare handle)
+        {
+            if (handle != null
+                && this.prepareCalled == 0)
+            {
+                this.prepareCalled++;
+
+                this.task = Task.Run(() => this.AsyncSend());
+            }
+        }
+
+        void AsyncCallback(Async handle)
+        {
+            if (handle != null)
+            {
+                int n = ++this.asyncCalled;
+
+                if (n == 3)
+                {
+                    this.prepare.Dispose();
+                    this.async.Dispose();
+                }
+            }
+        }
+
+        [Fact]
+        public void Async()
+        {
+            this.loop = new Loop();
+            this.prepareCalled = 0;
+            this.asyncCalled = 0;
+
+            this.prepare = this.loop.CreatePrepare();
+            this.prepare.Start(this.PrepareCallback);
+
+            this.async = this.loop.CreateAsync(this.AsyncCallback);
+
+            this.loop.RunDefault();
+
+            Assert.True(this.task.IsCompleted);
+            Assert.True(this.prepareCalled > 0, "Prepare callback should be called at least once.");
+            Assert.Equal(3, this.asyncCalled);
+        }
+
+        public void Dispose()
+        {
+            this.loop?.Dispose();
+            this.loop = null;
+        }
+    }
+}

StormHub/NetUV

src/NetUV.Core/Native/NativeLoop.cs
@@ -1,0 +1,213 @@
+﻿// Copyright (c) Johnny Z. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+// ReSharper disable InconsistentNaming
+
+namespace NetUV.Core.Native
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    enum uv_run_mode
+    {
+        UV_RUN_DEFAULT = 0,
+        UV_RUN_ONCE,
+        UV_RUN_NOWAIT
+    };
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct uv_loop_t
+    {
+        /* User data - use this for whatever. */
+        public IntPtr data;
+
+        /* Loop reference counting. */
+        public uint active_handles;
+    }
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate void uv_walk_cb(IntPtr handle, IntPtr arg);
+
+    static partial class NativeMethods
+    {
+        internal static int GetLoopSize()
+        {
+            IntPtr value = uv_loop_size();
+            int size = value.ToInt32();
+            if (size <= 0)
+            {
+                throw new InvalidOperationException("Loop handle size must be greater than zero.");
+            }
+
+            return size;
+        }
+
+        internal static void InitializeLoop(IntPtr handle)
+        {
+            if (handle == IntPtr.Zero)
+            {
+                throw new ArgumentException("Invalid empty handle value.", nameof(handle));
+            }
+
+            Invoke(uv_loop_init, handle);
+        }
+
+        internal static bool CloseLoop(IntPtr handle)
+        {
+            if (handle == IntPtr.Zero)
+            {
+                return false;
+            }
+
+            int result = uv_loop_close(handle);
+            if (result >= 0)
+            {
+                return true;
+            }
+
+            var code = (uv_err_code)result;
+            if (code == uv_err_code.UV_EBUSY)
+            {
+                return false;
+            }
+
+            throw CreateError(code);
+        }
+
+        internal static void WalkLoop(IntPtr handle, uv_walk_cb callback)
+        {
+            if (handle == IntPtr.Zero 
+                || callback == null)
+            {
+                return;
+            }
+
+            InvokeAction(uv_walk, handle, callback, handle);
+        }
+
+        internal static int RunLoop(IntPtr handle, uv_run_mode mode)
+        {
+            if (handle == IntPtr.Zero)
+            {
+                throw new ArgumentException("Empty handle value is not valid.");
+            }
+
+            /*
+              UV_RUN_DEFAULT: 
+                Runs the event loop until there are no more active and referenced handles or requests.
+                Returns non-zero if uv_stop() was called and there are still active handles or requests.
+                Returns zero in all other cases.
+
+              UV_RUN_ONCE: 
+                Poll for i/o once. Note that this function blocks if there are no pending callbacks.
+                Returns zero when done (no active handles or requests left), 
+                or non-zero if more callbacks are expected(meaning you should run the event loop again sometime in the future).
+
+              UV_RUN_NOWAIT: 
+                Poll for i/o once but don’t block if there are no pending callbacks.
+                Returns zero if done(no active handles or requests left), 
+                or non-zero if more callbacks are expected(meaning you should run the event loop again sometime in the future).
+            */
+
+            int result = InvokeFunction(uv_run, handle, mode);
+            Log.Debug($"Native run loop {handle} {mode}, result = {result}");
+            return result;
+        }
+
+        internal static void StopLoop(IntPtr handle)
+        {
+            if (handle == IntPtr.Zero)
+            {
+                throw new ArgumentException("Empty handle value is not valid.");
+            }
+
+            InvokeAction(uv_stop, handle);
+        }
+
+        internal static bool IsLoopAlive(IntPtr handle)
+        {
+            if (handle == IntPtr.Zero)
+            {
+                throw new ArgumentException("Empty handle value is not valid.");
+            }
+
+            int result = InvokeFunction(uv_loop_alive, handle);
+            return result != 0;
+        }
+
+        internal static long LoopNow(IntPtr handle)
+        {
+            if (handle == IntPtr.Zero)
+            {
+                throw new InvalidOperationException("Invalid empty handle value.");
+            }
+
+            return InvokeFunction(uv_now, handle);
+        }
+
+        internal static long LoopNowInHighResolution(IntPtr handle)
+        {
+            if (handle == IntPtr.Zero)
+            {
+                throw new InvalidOperationException("Invalid empty handle value.");
+            }
+
+            return InvokeFunction(uv_hrtime, handle);
+        }
+
+        internal static int GetBackendTimeout(IntPtr handle)
+        {
+            if (handle == IntPtr.Zero)
+            {
+                throw new InvalidOperationException("Invalid empty handle value.");
+            }
+
+            // The return value is in milliseconds, or -1 for no timeout.
+            int timeout = InvokeFunction(uv_backend_timeout, handle);
+            return timeout > 0 ? timeout : 0;
+        }
+
+        internal static void LoopUpdateTime(IntPtr handle)
+        {
+            if (handle == IntPtr.Zero)
+            {
+                throw new ArgumentException("Empty handle value is not valid.");
+            }
+
+            InvokeAction(uv_update_time, handle);
+        }
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        static extern int uv_loop_init(IntPtr handle);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        static extern void uv_stop(IntPtr handle);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        static extern int uv_loop_close(IntPtr handle);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        static extern int uv_loop_alive(IntPtr handle);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        static extern int uv_run(IntPtr handle, uv_run_mode mode);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        static extern IntPtr uv_loop_size();
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        static extern void uv_update_time(IntPtr handle);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        static extern long uv_now(IntPtr handle);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        static extern long uv_hrtime(IntPtr handle);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        static extern int uv_backend_timeout(IntPtr handle);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        static extern void uv_walk(IntPtr handle, uv_walk_cb walk_cb, IntPtr arg);
+    }
+}

test/NetUV.Tests/AsyncTests.cs
@@ -1,0 +1,79 @@
+﻿// Copyright (c) Johnny Z. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace NetUV.Core.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using NetUV.Core.Handles;
+    using Xunit;
+
+    public sealed class AsyncTests : IDisposable
+    {
+        Loop loop;
+        Prepare prepare;
+        Async async;
+        Task task;
+
+        int prepareCalled;
+        int asyncCalled;
+
+        void AsyncSend()
+        {
+            while (this.asyncCalled < 3)
+            {
+                this.async.Send();
+            }
+        }
+
+        void PrepareCallback(Prepare handle)
+        {
+            if (handle != null
+                && this.prepareCalled == 0)
+            {
+                this.prepareCalled++;
+
+                this.task = Task.Run(() => this.AsyncSend());
+            }
+        }
+
+        void OnAsync(Async handle)
+        {
+            if (handle != null)
+            {
+                int n = ++this.asyncCalled;
+
+                if (n == 3)
+                {
+                    this.prepare.Dispose();
+                    this.async.Dispose();
+                }
+            }
+        }
+
+        [Fact]
+        public void Async()
+        {
+            this.loop = new Loop();
+            this.prepareCalled = 0;
+            this.asyncCalled = 0;
+
+            this.prepare = this.loop.CreatePrepare();
+            this.prepare.Start(this.PrepareCallback);
+
+            this.async = this.loop.CreateAsync(this.OnAsync);
+
+            this.loop.RunDefault();
+
+            Assert.True(this.task.IsCompleted);
+            Assert.True(this.prepareCalled > 0, "Prepare callback should be called at least once.");
+            Assert.Equal(3, this.asyncCalled);
+        }
+
+        public void Dispose()
+        {
+            this.loop?.Dispose();
+            this.loop = null;
+        }
+    }
+}