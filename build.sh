d38093133f574fc034c440732a74202f001d9ff4
JohnnyZ <johnny.chow.19@gmail.com>
2020-08-25 07:58:50 +1000

    Build scripts.

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ echo $CONFIGURATION
 
 dotnet restore
 
-dotnet build -c $CONFIGURATION -f netstandard1.6 ./src/NetUV.Core/NetUV.Core.csproj
+dotnet build -c $CONFIGURATION -f netstandard2.1 ./src/NetUV.Core/NetUV.Core.csproj
 
-dotnet test -c $CONFIGURATION -f netcoreapp1.1 ./test/NetUV.Core.Tests/NetUV.Core.Tests.csproj
+dotnet test -c $CONFIGURATION -f netcoreapp3.1 ./test/NetUV.Core.Tests/NetUV.Core.Tests.csproj
 
-dotnet run -c $CONFIGURATION -f netcoreapp1.1 -p ./test/NetUV.Core.Tests.Performance/NetUV.Core.Tests.Performance.csproj
+dotnet run -c $CONFIGURATION -f netcoreapp3.1 -p ./test/NetUV.Core.Tests.Performance/NetUV.Core.Tests.Performance.csproj
